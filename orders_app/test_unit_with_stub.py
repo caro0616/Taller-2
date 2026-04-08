a967710153653188a18063941085f5ae1cb51a33
Leidy Obando <125570525+caro0616@users.noreply.github.com>
2026-04-08 12:37:03 +0000

    subido

---
+++ orders_app/test_unit_with_stub.py
@@ -0,0 +1,27 @@
+from order_service import create_order
+from database import SessionLocal
+from models import Order
+
+class StubUserRepository:
+    def get_user_email(self, user_id):
+        # TODO: retornar siempre el mismo email ficticio
+        return "stub@example.com"
+
+class DummyLogger:
+    def log(self, msg):
+        pass
+
+class NullNotifier:
+    def send(self, to, message):
+        pass
+
+def test_create_order_with_stub():
+    db = SessionLocal()
+    order = create_order(10, 200, NullNotifier(), DummyLogger(), db, StubUserRepository())
+    # TODO: Comparar con las respuestas esperadas
+    assert order.status == 'CREATED'
+    assert order.user_email == "stub@example.com"
+
+    db.query(Order).delete()
+    db.commit()
+    db.close()
